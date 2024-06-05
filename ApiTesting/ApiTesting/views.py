662300493ccfcb18fce26232daa5ddfe4ffa6524
Personal <76549372+sanketschendake@users.noreply.github.com>
2024-06-05 12:43:16 +0530

    Delete ApiTesting directory

--- ApiTesting/ApiTesting/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse, JsonResponse
-
-
-def home_page(request):
-    name = ['sanket', 'chendake']
-
-    # return HttpResponse('<h1>This is home page<h1>')
-    return JsonResponse(name, safe=False)
