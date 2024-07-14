e7f430c2ec38b744a9d4e2646824df5e044425b3
Personal <76549372+sanketschendake@users.noreply.github.com>
2024-07-14 13:19:20 +0530

    Delete scanner directory

--- scanner/myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('home/', views.home, name='home_url'),
-    path('genqr/', views.generate_qr),
-    path('fetch_external_data/', views.fetch_external_data),
-]
